b56c39dce706b793db38df6dcc2aef69bc5f5747
Joseph Morag <jm4157@columbia.edu>
2018-01-24 11:27:35 -0500

    Define more lilyval types

--- src/LilyVal.hs
+++ src/LilyVal.hs
@@ -3,6 +3,8 @@
 
 
 module LilyVal (
+    PitchClass(..),
+    Articulation(..)
 ) where
 
 
@@ -13,7 +15,7 @@ import           Control.Exception
 import           Control.Monad.Reader
 
 
-data Pitchclass = C | Dff | Btqs | Cqs | Dtqf | Bss | Cs | Df | Ctqs 
+data PitchClass = C | Dff | Btqs | Cqs | Dtqf | Bss | Cs | Df | Ctqs 
                 | Dqf | Css | D | Eff | Dqs | Etqf | Ds | Ef | Fff 
                 | Dtqs | Eqf | Ftqf | Dss | E | Ff | Eqs | Fqf | Es 
                 | F | Gff | Etqs | Fqs | Gtqf | Ess | Fs | Gf | Ftqs 
@@ -21,3 +23,25 @@ data Pitchclass = C | Dff | Btqs | Cqs | Dtqf | Bss | Cs | Df | Ctqs
                 | Gss | A | Bff | Aqs | Btqf | As | Bf | Cff | Atqs | Bqf 
                 | Ctqf | Ass | B | Cf | Bqs | Cqf | Bs
 
+data Articulation = (!) | (-) | (.) | (>) | (+) | (^) 
+
+type Dur = Rational
+type Octave = Int
+
+-- E | A | D | G for violin, but general for all strings
+data String = One | Two | Three | Four 
+type Harmonic = Bool
+data Finger = Open | One | Two | Three | Four
+
+data Tempo = Tempo Dur Int
+data Fingering = Fingering Finger Harmonic String
+
+data Note = Note { pitch  :: PitchClass
+                 , dur    :: Dur
+                 , oct    :: Octave
+                 , art    :: Maybe Articulation
+                 , tempo  :: Tempo
+                 , finger :: Fingering
+                 }
+
+-- TODO chords and slurs and polyphony
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import           Lilyval
 main :: IO ()
 main = do
   putStrLn "hello world"
+
