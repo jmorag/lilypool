75331ccc5d13f1827a63b68ae3bbda5df5204614
Joseph Morag <jm4157@columbia.edu>
2018-02-07 00:34:04 -0500

    Significant progress on parser
    Refactor PitchClass into BaseNote type and Accidental types. School of
    Haskell enumerates them all in order to derive Ord, but this is not
    really necessary, since enharmonic equivalents will be converted to the
    same double under the hood anyway.

--- src/Lilyval.hs
+++ src/Lilyval.hs
@@ -13,13 +13,23 @@ import           Control.Monad.Reader
 import           Prelude hiding (String)
 
 
-data PitchClass = C | Dff | Btqs | Cqs | Dtqf | Bss | Cs | Df | Ctqs 
-                | Dqf | Css | D | Eff | Dqs | Etqf | Ds | Ef | Fff 
-                | Dtqs | Eqf | Ftqf | Dss | E | Ff | Eqs | Fqf | Es 
-                | F | Gff | Etqs | Fqs | Gtqf | Ess | Fs | Gf | Ftqs 
-                | Gqf | Fss | G | Aff | Gqs | Atqf | Gs | Af | Gtqs | Aqf 
-                | Gss | A | Bff | Aqs | Btqf | As | Bf | Cff | Atqs | Bqf 
-                | Ctqf | Ass | B | Cf | Bqs | Cqf | Bs
+-- data PitchClass = C | Dff | Btqs | Cqs | Dtqf | Bss | Cs | Df | Ctqs 
+--                 | Dqf | Css | D | Eff | Dqs | Etqf | Ds | Ef | Fff 
+--                 | Dtqs | Eqf | Ftqf | Dss | E | Ff | Eqs | Fqf | Es 
+--                 | F | Gff | Etqs | Fqs | Gtqf | Ess | Fs | Gf | Ftqs 
+--                 | Gqf | Fss | G | Aff | Gqs | Atqf | Gs | Af | Gtqs | Aqf 
+--                 | Gss | A | Bff | Aqs | Btqf | As | Bf | Cff | Atqs | Bqf 
+--                 | Ctqf | Ass | B | Cf | Bqs | Cqf | Bs
+
+-- This is less disgusting than the above
+data BaseNote = A | B | C | D | E | F | G 
+    deriving Enum
+
+data Accidental = DoubleFlat | TQFlat | Flat | QFlat 
+                | QSharp | Sharp | TQSharp | DoubleSharp
+    deriving Enum
+
+data PitchClass = PitchClass BaseNote Accidental
 
 -- ! . ^ > - -. +
 data Articulation = Staccatissimo | Staccato | VAccent | Accent 
--- src/Parser.hs
+++ src/Parser.hs
@@ -7,13 +7,104 @@ import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 import           Data.Text (Text)
 import qualified Data.Text as T
-import Control.Monad (void)
+import Control.Monad (void, foldM)
 import Data.Void
+
+import           Data.Array (listArray, (!))
+import           Data.Maybe (fromMaybe)
 
 import Lilyval
 
 type Parser = Parsec Void Text
 
--- pitchClassP :: Parser PitchClass
--- pitchClassP =  do
---     oneOf "abcdef"
+{- 
+ - Many examples taken from 
+ - https://markkarpov.com/megaparsec/parsing-simple-imperative-language.html
+ -}
+
+-- define space consumer
+spaceConsumer :: Parser ()
+spaceConsumer = L.space exceptNewlines lineCmnt blockCmnt
+  where
+    lineCmnt  = L.skipLineComment "%"
+    blockCmnt = empty
+    -- newLines mean the end of a measure, so they are syntactically relevant
+    exceptNewlines = skipSome $ oneOf ['\t', ' ', '\v']
+
+-- This consumes all whitespace after every lexeme, but not before
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme spaceConsumer
+
+-- Augment the symbol parser to consume whitespace after a fixed symbol
+symbol :: Text -> Parser Text 
+symbol = L.symbol spaceConsumer
+
+-- Parse the reserved time, key, and tempo symbols
+keyP :: Parser Key
+keyP = do 
+    symbol "#key"
+    pitchClass <- pitchClassP
+    quality <- qualityP
+    spaceConsumer
+    eol
+    return $ Key pitchClass quality
+
+qualityP :: Parser Quality
+qualityP = do
+    qualityName <- char' 'm'
+    case qualityName of
+        'm' -> return Minor
+        'M' -> return Major
+
+timeP :: Parser Time
+timeP = do
+    symbol "#time"
+    -- verify at parse time that the rational has a denominator that is a power
+    -- of two
+    numerator <- L.decimal
+    char '/'
+    denominator <- L.decimal
+    if isNotPowerofTwo denominator then error "Denominator not power of two" else
+        return $ Time (numerator % denominator)
+    where
+        isNotPowerofTwo n = logBase 2.0 (fromIntegral denominator)
+
+    
+
+
+tempoP = symbol "#tempo"
+
+
+-- Parse a pitchclass
+pitchClassP :: Parser PitchClass
+pitchClassP = do
+    baseNote <- baseNoteP
+    accidental <- accidentalP
+    return $ PitchClass baseNote accidental
+
+
+-- baseNoteP and accidentalP use arrays and maps, respectively as lookup tables
+-- we should just pick one and stick with it
+baseNoteP :: Parser BaseNote
+baseNoteP = do
+    noteName <- satisfy (\c -> c `elem` ['a'..'g']) 
+    let noteMap = listArray ('a', 'g') [A .. G]
+    return $ noteMap ! noteName
+    
+
+accidentalP :: Parser Accidental
+accidentalP = do
+    let accidentalStrs = ["ff", "tqf", "f", "qf", "qs", "s", "tqs", "ss"]
+    accidentalName <- 
+        -- there has to be a way to do something like this fold, but 
+        -- for now, we can be satisfied with this amount of redundant typing
+        -- foldr ((<|>)) $ map string accidentalStrs
+        string "ff"  <|> string "tqf" <|>
+        string "f"   <|> string "qf"  <|>
+        string "qs"  <|> string "s"   <|>
+        string "tqs" <|> string "ss"
+    let accidentalMap = zip accidentalStrs [DoubleFlat .. DoubleSharp]
+        -- the accidentalName parser would fail before this partial match would
+        Just accidental = lookup accidentalName accidentalMap
+    return accidental
+
